b34869773d6d0427320a9e5e8058218b639e8dfa
Jennie <jennie.lifvendahl@live.se>
2021-03-18 14:13:36 +0100

    First commit

---
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -0,0 +1,31 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+
+    @Given("I have navigated to the item site on Clas Ohlson")
+    public void i_have_navigated_to_the_item_site_on_clas_ohlson() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("I have selected {int} item\\(s) to the cart")
+    public void i_have_selected_item_s_to_the_cart(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I click on add to the cart")
+    public void i_click_on_add_to_the_cart() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the result should be {int} item\\(s) in the cart")
+    public void the_result_should_be_item_s_in_the_cart(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ target/test-classes/Features/addition.feature
@@ -0,0 +1,12 @@
+Feature: Clas Ohlson's cart
+  Put 2 item(s) of glue in the cart
+
+  Scenario: Add 2 glue(s) to the Clas Ohlson cart
+    Given I have navigated to the item site on Clas Ohlson
+    And I have selected 2 item(s) to the cart
+    When I click on add to the cart
+    Then the result should be 2 item(s) in the cart
+
+
+
+
